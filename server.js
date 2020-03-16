a307d8a87b2419d305d4dcd85781343267298683
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-16 16:02:15 +0000

    Not finished.

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
+server.use(db);
 server.use(express.json());
+
+
+server.get('/', (req, res) =>{
+    db.select('*').from('Customers').where({ postalcode: '1010' });
+});
+
+server.get('/', (req, res) => {
+    db.select('*').from('Orders').where({ supplierID: 11});
+})
+
+server.get('/', (req, res) => {
+    db.select('*').from('Orders').orderBy('OrderDate')
+})
+
+server.post('/', (req, res) => {
+    db.select('Customers').where({ City: 'London'}).orWhere({ City: 'Madrid'}).orWhere({ Country: 'Brazil'});
+})
+
+server.put('/update', (req, res) => {
+    
+});
+
 
 module.exports = server;
